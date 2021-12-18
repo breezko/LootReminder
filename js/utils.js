4d763083a85fa26f62ff85ade39ae3a7a77cb889
Breezko <hendrik.heim@gmx.de>
2021-12-18 23:35:40 +0100

    Add audio, fix logic error

--- js/utils.js
+++ js/utils.js
@@ -6,18 +6,25 @@ var lootIntervalElement = document.getElementById("loot-interval");
 var runTimerElement = document.getElementById('start');
 var alertOneElement = document.getElementById('alert-1');
 var alertTwoElement = document.getElementById('alert-2');
+var playAudio = document.getElementById('play-audio-check');
+var audioVolume = document.getElementById('volume-value');
 
+var song = new Audio();
+song.src = 'alert.mp3';
+audioVolume.onchange = function() {
+    song.volume = this.value / 10;
+}
 
 function start() {
     var x, alertOne, alertTwo;
     var total = 0;
     const TIMEOUT = 120
     running = !running;
-    if (alertOneElement.value.length > 1 && alertTwoElement.value.length > 1) {
+
+    if (alertOneElement.value.length > 0 && alertTwoElement.value.length > 0) {
         alertOne = parseInt(alertOneElement.value) / TIMEOUT;
         alertTwo = parseInt(alertTwoElement.value) / TIMEOUT;
-        console.log(alertOne);
-        console.log(alertTwo);
+
     }
     else {
         alertOne = 0.84;
@@ -27,7 +34,11 @@ function start() {
         percentage = (total % TIMEOUT) / TIMEOUT;
         lootIntervalElement.style.width = percentage * 100 + "%";
         lootIntervalElement.innerText = (total % TIMEOUT);
-
+        if(percentage == alertOne || percentage == alertTwo){
+            if (playAudio.checked) {
+                song.play()
+            }
+        }
     
         if (percentage < alertOne) {
             lootIntervalElement.style.backgroundColor = "green";
